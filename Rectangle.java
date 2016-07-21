b6187c91499f6f4f9457cf2e540dd561ed8f6171
tanimu <liva.soul@gmail.com>
2016-07-22 02:03:37 +0900

    first commit

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+package ch11_7;
+class Rectangle {
+    int width;
+    int height;
+    void setSize(int w, int h) {
+        width = w;
+        height = h;
+    }
+    int getArea() {
+        return width * height;
+    }
+    public static String toString(int width, int height){
+    	return "Rectangle (" + width +","+ height +")";
+    }
+
+
+    public static void main(String[] args){
+    	Rectangle r = new Rectangle();
+    	r.setSize(123,45);
+    	System.out.println(toString(r.width,r.height));
+    }
+}
